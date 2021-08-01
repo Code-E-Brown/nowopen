99575a1d51ad4b464f5e1c02dbcc06cdfdb95eef
cody <cody.brown1993@gmail.com>
2021-07-31 23:57:23 -0400

    REVERSE GEOCODE WORKS!!

--- react-app/src/components/BusinessAddress/index.js
+++ react-app/src/components/BusinessAddress/index.js
@@ -5,15 +5,17 @@ function Address({ currentBusiness, currentLat, currentLong, apiKey }) {
     const [address, setAddress] = useState('')
 
 
-    // useEffect(async () => {
-    //     const result = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${+currentBusiness.currentBusiness.current_lat},${+currentBusiness.currentBusiness.current_long}&key=${apiKey}`)
-    //     console.log('**************L OOKS', result)
-    // }, [])
+    useEffect(async () => {
+        const result = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${+currentBusiness.currentBusiness.current_lat},${+currentBusiness.currentBusiness.current_long}&key=${apiKey}`)
+        const json = await result.json()
+        console.log('********', json.results[0])
+        setAddress(json.results[0].formatted_address)
+    }, [])
 
     return (
         <a href={`https://www.google.com/maps/dir/${+currentBusiness.current_lat},${+currentBusiness.current_long}/${currentLat},${currentLong}`} target="_blank" rel="noopener noreferrer">
             {/* Located at: 555 E street, Washington D.C */}
-            Get directions!
+            Located at: {address}
         </a>
     )
 }
