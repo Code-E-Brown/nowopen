32e9e3a7bd153353a5a555e9d05ffcc19e73a19f
cody <cody.brown1993@gmail.com>
2021-08-07 17:16:24 -0400

    accounted for no location on address fetcher

--- react-app/src/components/BusinessAddress/index.js
+++ react-app/src/components/BusinessAddress/index.js
@@ -13,11 +13,18 @@ function Address({ currentBusiness, currentLat, currentLong, apiKey }) {
     }, [])
 
     return (
-        // <a href={`https://www.google.com/maps/dir/${+currentBusiness.current_lat},${+currentBusiness.current_long}/${currentLat},${currentLong}`} target="_blank" rel="noopener noreferrer">
-        <a href={`https://www.google.com/maps/dir/${currentLat},${currentLong}/${+currentBusiness.current_lat},${+currentBusiness.current_long}`} target="_blank" rel="noopener noreferrer">
-            {/* Located at: 555 E street, Washington D.C */}
-            Located at: {address}
-        </a>
+        <>
+            {/* <a href={`https://www.google.com/maps/dir/${+currentBusiness.current_lat},${+currentBusiness.current_long}/${currentLat},${currentLong}`} target="_blank" rel="noopener noreferrer"> */}
+            {currentLat && currentLong ? (
+
+                <a href={`https://www.google.com/maps/dir/${currentLat},${currentLong}/${+currentBusiness.current_lat},${+currentBusiness.current_long}`} target="_blank" rel="noopener noreferrer">
+                    {/* Located at: 555 E street, Washington D.C */}
+                    Located at: {address}
+                </a>
+            ) : (<div>Located at: {address}
+            </div>)
+            }
+        </>
     )
 }
 export default Address
